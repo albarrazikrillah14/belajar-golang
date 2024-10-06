aa928906d9222c1888d84eea19fc989e1730da7c
albarrazikrillah14 <albarrazikrillah1405@gmail.com>
2024-10-06 23:51:03 +0700

    v

---
+++ fmt.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstname := "Albarra"
+	lastname := "Zikrillah"
+
+	fmt.Printf("Hello %s %s!\n", firstname, lastname)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module belajar-golang-std-lib
+
+go 1.23.1
